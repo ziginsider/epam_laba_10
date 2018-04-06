e231df4f7e79818bf021cb0609c7530f372871ef
ziginsider <ziginsider@gmail.com>
2018-04-06 23:57:43 +0300

    create CustomView class

--- app/src/main/java/io/github/ziginsider/epam_laba_10/CustomView.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba_10/CustomView.kt
@@ -1,7 +1,26 @@
 package io.github.ziginsider.epam_laba_10
 
+import android.annotation.TargetApi
+import android.content.Context
+import android.graphics.Canvas
+import android.os.Build
+import android.util.AttributeSet
 import android.view.View
 
-class CustomView : View() {
-    
+class CustomView : View {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int)
+            : super(context, attrs, defStyleAttr)
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int)
+            : super(context, attrs, defStyleAttr, defStyleRes)
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+    }
 }
